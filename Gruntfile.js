ba74ab9d594675c34dfcc2b5b86fc4a2b02fd06f
Eric Defore <eric.defore@gmail.com>
2015-11-20 17:35:23 -0500

    Converted Plugin to Class structure. Need to figure out why conditional script loading is not working.

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,13 +13,39 @@ module.exports = function( grunt ) {
             options: {
                 livereload: true
             },
-            admin: {
+            admin_js: {
                 files: [ 'build/js/admin/*.js' ],
-                tasks: [ 'uglify:admin', 'notify:admin' ]
+                tasks: [ 'uglify:admin_js', 'notify:admin_js' ]
             },
-            frontend: {
+            frontend_js: {
                 files: [ 'build/js/frontend/*.js' ],
-                tasks: [ 'uglify:frontend', 'notify:frontend' ]
+                tasks: [ 'uglify:frontend_js', 'notify:frontend_js' ]
+            },
+            frontend_sass: {
+                files: [ 'build/sass/frontend/*.scss' ],
+                tasks: [ 'sass:frontend_sass', 'autoprefixer:frontend_sass', 'notify:frontend_sass' ]
+            }
+        },
+
+        // SASS
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            frontend_sass: {
+                files: {
+                    'css/simple-slider.css': 'build/sass/frontend/simple-slider.scss'
+                }
+            }
+        },
+
+        // Auto prefix our CSS with vendor prefixes
+        autoprefixer: {
+            options: {
+                map: true
+            },
+            frontend_sass: {
+                src: 'css/simple-slider.css'
             }
         },
 
@@ -28,12 +54,12 @@ module.exports = function( grunt ) {
             options: {
                 sourceMap: true
             },
-            admin: {
+            admin_js: {
                 files: {
                     'js/admin/simple-slider-admin.js': [ 'build/js/admin/*.js' ]
                 }
             },
-            frontend: {
+            frontend_js: {
                 files: {
                     'js/frontend/simple-slider.js': [ 'build/js/frontend/*.js' ]
                 }
@@ -41,16 +67,22 @@ module.exports = function( grunt ) {
         },
 
         notify: {
-            admin: {
+            admin_js: {
                 options: {
                     title: '<%= pkg.name %>',
                     message: 'JS Admin Complete'
                 }
             },
-            frontend: {
+            frontend_js: {
                 options: {
                     title: '<%= pkg.name %>',
                     message: 'JS Frontend Complete'
+                }
+            },
+            frontend_sass: {
+                options: {
+                    title: '<%= pkg.name %>',
+                    message: 'SASS Frontend Complete'
                 }
             }
         }
--- real-big-simple-slider.php
+++ real-big-simple-slider.php
@@ -7,28 +7,112 @@ Author: Eric Defore
 License: GPL
 */
 
-add_action( 'admin_init', 'add_slider_button_filters' );
-function add_slider_button_filters() {
+class RealBigSlider {
+
+    static $add_frontend_styles_scripts;
+
+    static function init() {
+
+        // Admin
+        add_action( 'admin_init', array( __CLASS__, 'add_slider_button_filters' ) );
+
+        // Frontend
+        add_shortcode( 'realbig_slider', array( __CLASS__, 'realbig_slider_shortcode_register' ) );
+        add_action( 'init', array( __CLASS__, 'register_slider_styles_scripts' ) );
+        add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_slider_styles_scripts' ) );
+
+    }
+
+
+    static function add_slider_button_filters() {
+
+        if ( current_user_can( 'edit_posts' ) && current_user_can( 'edit_pages' ) ) {
+
+            add_filter( 'mce_buttons', array( __CLASS__, 'register_slider_button' ) );
+
+            // Script is attached to the button, rather than registered/enqueued regularly.
+            add_filter( 'mce_external_plugins', array( __CLASS__, 'add_slider_button' ) );
+
+        }
+
+    }
+
+    static function register_slider_button( $buttons ) {
+
+        array_push( $buttons, 'slider_button' );
+        return $buttons;
+
+    }
+
+    static function add_slider_button( $plugin_array ) {
+
+        $plugin_array['slider_button_script'] = plugins_url( '/js/admin/simple-slider-admin.js', __FILE__ ) ;
+        return $plugin_array;
+
+    }
+
+    static function realbig_slider_shortcode_register( $atts, $content ){
+
+        self::$add_frontend_styles_scripts = true;
+
+        $atts = shortcode_atts(
+            array(// a few default values
+                'ids' => '',
+                'arrows' => true,
+                'classes' => '',
+            ),
+            $atts,
+            'realbig_slider'
+        );
+
+        if ( $atts['ids'] == '' ) {
+            return 'Please Select Attachment IDs for the Slider.';
+        }
+
+        $attachment_ids = explode( ',', $atts['ids'] );
+
+        $out = '';
+
+        $out .= '<div class = "realbig-slider' . ( ( $atts['classes'] != '' ) ? ' ' . $atts['classes'] : '' ) . '">';
+
+            $out .= '<ul>';
+
+            foreach ( $attachment_ids as $id ) {
+
+                $out .= '<li>' . wp_get_attachment_image( $id, 'full' ) . '</li>';
+
+            }
+
+            $out .= '</ul>';
+
+        $out .= '<a href = "#" class = "previous">Previous</a>';
+
+        $out .= '<a href = "#" class = "next">Next</a>';
+
+        $out .= '</div>';
+
+        return html_entity_decode( $out );
+
+    }
+
+    static function register_slider_styles_scripts() {
+
+        wp_register_style( 'realbig-simple-slider-css', plugins_url( '/css/simple-slider.css', __FILE__ ) );
+        wp_register_script( 'realbig-simple-slider-js', plugins_url( '/js/frontend/simple-slider.js', __FILE__ ), array( 'jquery' ), true );
+
+    }
     
-     if ( current_user_can( 'edit_posts' ) && current_user_can( 'edit_pages' ) ) {
-         
-          add_filter( 'mce_buttons', 'register_slider_button' );
-          add_filter( 'mce_external_plugins', 'add_slider_button' );
-         
-     }
-    
+    static function enqueue_slider_styles_scripts() {
+
+        if ( self::$add_frontend_styles_scripts !== true ) {
+         //   return;
+        }
+
+        wp_enqueue_style( 'realbig-simple-slider-css' );
+        wp_enqueue_script( 'realbig-simple-slider-js' );
+
+    }
+
 }
 
-function register_slider_button( $buttons ) {
-    
-     array_push( $buttons, 'slider_button' );
-     return $buttons;
-    
-}
-
-function add_slider_button( $plugin_array ) {
-    
-     $plugin_array['slider_button_script'] = plugins_url( '/js/admin/simple-slider-admin.js', __FILE__ ) ;
-     return $plugin_array;
-    
-}
+RealBigSlider::init();
